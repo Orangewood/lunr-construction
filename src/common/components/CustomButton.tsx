fb1ad01818ecc37c047e5a82a5916dc66a1e8938
James Dolan <jamesdolan1989@gmail.com>
2020-09-24 21:08:31 -0400

    Switchable button props

--- src/common/components/CustomButton.tsx
+++ src/common/components/CustomButton.tsx
@@ -6,27 +6,38 @@ import GreenButton from "../images/GreenButton.svg";
 
 import "../scss/Button.scss";
 
+
+
 export type ButtonClass = {
-  orange: boolean;
-  blue: boolean;
-  green: boolean;
+  orange: string;
+  blue: string;
+  green: string;
+};
+
+export const ButtonColors: ButtonClass = {
+    orange: "orange",
+    blue: "blue",
+    green: "green",
 };
 
 interface CustomButtonProps {
   text: string;
-  color?: ButtonClass;
+  color?: string;
   onClickedButton: (clicked: boolean) => void;
 }
 
+
 export default function CustomButton(props: CustomButtonProps): JSX.Element {
-  const {text, color, onClickedButton} = props;
+  const { text, color, onClickedButton } = props;
   return (
     <>
-    <button className="custom-button">
-      <div className="button-text">Sign In</div>
-    {/* <img src={GreenButton} onClick={() => onClickedButton(true)} /> */}
-    </button>
-
+      <button
+        className={`custom-button-${color}`}
+        onClick={() => console.log("hi")}
+      >
+        <div className='button-text'>{text}</div>
+        {/* <img src={GreenButton} onClick={() => onClickedButton(true)} /> */}
+      </button>
     </>
   );
 }
