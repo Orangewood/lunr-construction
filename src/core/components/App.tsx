66a09cf23950f35f991bcf11abcd943a1e8abb4a
James Dolan <jamesdolan1989@gmail.com>
2020-10-08 18:39:01 -0400

    Circle componenet for kiosk

--- src/core/components/App.tsx
+++ src/core/components/App.tsx
@@ -3,14 +3,22 @@ import IdolModal from "./kiosk/IdolModal";
 import KioskApp from "./kiosk/KioskApp";
 import AdminMenu from "./admin/AdminMenu";
 import ClockModal from "./kiosk/ClockModal";
+import ProgressBar from "./kiosk/progress-bar/components/ProgressBar"
 
 export default function App() {
   return (
     <>
       {/* <AdminMenu /> */}
-      <KioskApp />
+      {/* <KioskApp /> */}
       {/* <IdolModal onClickedSignOut={() => console.log('test')} onClickedYes={() => console.log('test')}/> */}
       {/* <ClockModal /> */}
+      <ProgressBar 
+          progress={80}
+          size={200}
+          strokeWidth={15}
+          circleOneStroke='#d9edfe'
+          circleTwoStroke={"red"}
+        />
     </>
   );
 }
